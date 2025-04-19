02114d98d0fae12c82ad9aef523fe612ff6d3ebb
Lukas Jarosch <lukas.jarosch@mail.com>
2025-04-19 14:41:36 +0200

    feat: setting some basic stuff

--- config/default.nix
+++ config/default.nix
@@ -1,4 +1,12 @@
 {
   # Import all your configuration modules here
   imports = [ ./bufferline.nix ./treesitter.nix  ./git.nix ./bufferline.nix];
+
+  colorschemes.rose-pine.enable = true;
+  colorschemes.rose-pine.settings.dark_variant = "moon";
+
+  plugins.trouble.enable = true;
+  plugins.neo-tree = {
+    enable = true;
+  };
 }
