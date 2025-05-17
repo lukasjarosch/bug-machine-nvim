3f8289d6927e4a042ed82e7a5cd7e1b7c6261597
Lukas Jarosch <lukas.jarosch@mail.com>
2025-05-17 12:16:40 +0200

    feat(plugin): add indent-blankline
    Signed-off-by: Lukas Jarosch <lukas.jarosch@mail.com>

---
+++ plugins/indent-blankline.nix
@@ -0,0 +1,17 @@
+{
+  plugins.indent-blankline = {
+    enable = true;
+
+    settings = {
+        scope = {
+            enabled = true;
+            show_end = true;
+            show_start = true;
+        };
+
+        indent = {
+          char = "│";
+        };
+    };
+  };
+}
