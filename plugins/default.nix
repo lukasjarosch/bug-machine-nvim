98ba4df33c126c71cfec3722a72ab01217631b84
Lukas Jarosch <lukas.jarosch@mail.com>
2026-03-26 09:39:45 +0100

    chore: format

--- plugins/default.nix
+++ plugins/default.nix
@@ -1,22 +1,15 @@
 {
   # Import all .nix files in the plugins directory
-  imports =
-    let
-      # Get all files in the current directory
-      files = builtins.readDir ./.;
-      # Filter for .nix files that aren't default.nix
-      nixFiles = builtins.filter
-        (filename:
-          builtins.match ".*\\.nix" filename != null &&
-          filename != "default.nix"
-        )
-        (builtins.attrNames files);
-      # Convert filenames to import paths
-      imports = map
-        (filename: ./${filename})
-        nixFiles;
-    in
-    imports;
+  imports = let
+    # Get all files in the current directory
+    files = builtins.readDir ./.;
+    # Filter for .nix files that aren't default.nix
+    nixFiles = builtins.filter (filename:
+      builtins.match ".*\\.nix" filename != null && filename != "default.nix")
+      (builtins.attrNames files);
+    # Convert filenames to import paths
+    imports = map (filename: ./${filename}) nixFiles;
+  in imports;
 
   plugins = {
     # nerdfont support
@@ -25,11 +18,7 @@
     copilot-lua = {
       enable = true;
       settings = {
-        panel = {
-          layout = {
-            position = "right";
-          };
-        };
+        panel = { layout = { position = "right"; }; };
         suggestion = {
           enabled = false;
           auto_trigger = false;
@@ -44,6 +33,5 @@
     vimGo.enable = true;
     nvim-surround.enable = true;
   };
-
 
 }
