bcde91f20d22431958d2de35e715616e570ed385
Lukas Jarosch <lukas.jarosch@mail.com>
2026-05-18 15:44:19 +0200

    feat(plugins/lsp-signature): enable lsp-signature

---
+++ plugins/lsp-signature.nix
@@ -0,0 +1,17 @@
+{
+  plugins.lsp-signature = {
+    enable = true;
+    settings = {
+      always_trigger = true;
+      bind = true;
+      floating_window = true;
+      handler_opts = {
+        border = "rounded";
+      };
+      hint_enable = true;
+      hint_prefix = "󱄑 ";
+      padding = "";
+      transparency = 10;
+    };
+  };
+}
